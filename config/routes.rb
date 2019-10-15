cd12fa5991b267b12d1f42c295b113d3f1a40633
Héctor Espinoza <hespinoza@uchile.cl>
2019-10-15 10:11:36 -0300

    first 13 steps finished

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :complaints, except: [:create]
+  resources :companies do
+    resources :complaints, only: [:create]
+  end
+  devise_for :users
+  root to: 'companies#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
