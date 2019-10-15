40a625ae5b19adec1a200d4a6f7d7ca189a622d5
Héctor Espinoza <hespinoza@uchile.cl>
2019-10-15 15:18:33 -0300

    admin link added

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,4 +30,4 @@ end
 
 end
 
-AdminUser.create!(email: "admin@example.com", password: "123456", password_confirmation: "123456") if Rails.env.development? 
+AdminUser.create!(email: "admin@example.com", password: "123456", password_confirmation: "123456")
